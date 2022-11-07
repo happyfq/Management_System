de570f553d8ef23933d2471ed210eb8d1a8803c9
happyfq <651408115@qq.com>
2022-11-06 23:04:47 -0800

    menu

---
+++ src/main/java/com/qingge/springboot/service/IRoleService.java
@@ -0,0 +1,16 @@
+package com.qingge.springboot.service;
+
+import com.qingge.springboot.entity.Role;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Fangqi
+ * @since 2022-11-06
+ */
+public interface IRoleService extends IService<Role> {
+
+}
